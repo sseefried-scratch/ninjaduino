2c83a481fdc1c170b9e77cc027c704d1f583d4af
Marcus Schappi <marcus@schappi.com>
2012-02-12 17:44:27 +1100

    working JSON, yay no more memory leak

--- ninja.ino
+++ ninja.ino
@@ -1,6 +1,7 @@
 #include <I2C.h>
 #include <Wire.h>
 #include <Sensor.h>
+#include <aJSON.h>
 
 int IDPinCon1 = A3;
 int IDPinCon2 = A6;
@@ -69,6 +70,30 @@ const byte FULL_SCALE_RANGE_8g = 0x2;
  
 const byte I2C_ADDR = 0x1D; //SA0=0
  
+
+const prog_char PROGMEM RETRIEVING_NAME[] ="Retrieving name\n";
+const prog_char PROGMEM ERROR_RETRIEVING_NAME[] ="Error retrieving name\n";
+const prog_char PROGMEM SUCCESSFULLY_RETRIEVED_NAME[] ="Successfully retrieved Name:";
+const prog_char PROGMEM PARSING_OBJECT[] ="Parsing String\n";
+const prog_char PROGMEM ERROR_PARSING_OBJECT[] ="Error parsing Object\n";
+const prog_char PROGMEM SUCCESSFULLY_PARSED_OBJECT[] ="Successfully parsed Object\n";
+const prog_char PROGMEM DELETING_OBJECT_STRING[] = "Deleting the object\n";
+const prog_char PROGMEM FORMAT_FAILED_STRING[] = "Failed to create Format Object\n";
+const prog_char PROGMEM OUTPUT_STRING_ERROR[] = "Error creating output String\n";
+const prog_char PROGMEM RESULT_PRINTING_STRING[] = "Printing the result:\n";
+const prog_char PROGMEM ADDING_FRAMERATE_STRING[] = "Adding frame rate to the format\n";
+const prog_char PROGMEM ADDING_INTERLACE_STRING[] = "Adding interlace to the format\n";
+const prog_char PROGMEM ADDING_HEIGHT_STRING[] = "Adding height to the format\n";
+const prog_char PROGMEM ADDING_WIDTH_STRING[] = "Adding width to the format\n";
+const prog_char PROGMEM ADDING_TYPE_STRING[] = "Adding type to the format\n";
+const prog_char PROGMEM ADDING_FORMAT_STRING[] = "Adding format to the object\n";
+const prog_char PROGMEM ADDING_LENGTH_STRING[] = "Adding length to the object\n";
+const prog_char PROGMEM CREATING_FROMAT_STRING[] = "Creating format object\n";
+const prog_char PROGMEM ADDING_NAME_STRING[] = "Adding name to the object\n";
+const prog_char PROGMEM OBJECT_CREATION_FAILED_STRING[] = "Failed to create the object\n";
+const prog_char PROGMEM OBJECT_CREATE_STRING[] = "Created a Object\n";
+const prog_char PROGMEM HELLO_STRING[] = "********************\nTesting aJson\n*****************\n";
+
 /*
   Read register content into buffer.
   The default count is 1 byte.
@@ -195,41 +220,152 @@ void setup()
   //Serial.print("WHO_AM_I=");
   //Serial.println(b,HEX);
 }
- 
+int x = 0, y = 0, z = 0;
+void testObjects() {
+
+  aJsonObject* root = aJson.createObject();
+  if (root != NULL) {
+    printProgStr( OBJECT_CREATE_STRING);
+  } 
+  else {
+    printProgStr( OBJECT_CREATION_FAILED_STRING);
+    return;
+  }
+  printProgStr( ADDING_NAME_STRING);
+  aJson.addItemToObject(root, "temperature", aJson.createItem(sen.getTemperature()));
+  printProgStr( CREATING_FROMAT_STRING);
+  aJsonObject* ports = aJson.createObject();
+  //if (fmt != NULL) {
+  //  printProgStr( ADDING_FORMAT_STRING);
+  //  aJson.addItemToObject(root, "format", fmt);
+  //  printProgStr( ADDING_TYPE_STRING);
+  //  aJson.addStringToObject(fmt, "type", "rect");
+  //  printProgStr( ADDING_WIDTH_STRING);
+  //  aJson.addNumberToObject(fmt, "width", 1920);
+  //  printProgStr( ADDING_HEIGHT_STRING);
+  //  aJson.addNumberToObject(fmt, "height", 1080);
+  //  printProgStr( ADDING_INTERLACE_STRING);
+  //  aJson.addFalseToObject(fmt, "interlace");
+  //  printProgStr( ADDING_FRAMERATE_STRING);
+  //  aJson.addNumberToObject(fmt, "frame rate", 24);
+  //  printProgStr( ADDING_LENGTH_STRING);
+  //  aJson.addNumberToObject(fmt, "length", 1.29);
+  //} 
+  //else {
+  //  printProgStr( FORMAT_FAILED_STRING);
+  //  return;
+  //}
+
+  freeMem("with object");
+  printProgStr( RESULT_PRINTING_STRING);
+  char* string = aJson.print(root);
+  if (string != NULL) {
+    Serial.println(string);
+  } 
+  else {
+    printProgStr( OUTPUT_STRING_ERROR);
+  }
+
+  printProgStr( DELETING_OBJECT_STRING);
+  aJson.deleteItem(root);
+  free(string);
+  freeMem("after deletion");
+}
+
 void loop()
 {
-  int x = 0, y = 0, z = 0;
- 
-  getAccXYZ(&x, &y, &z); //get accelerometer readings in normal mode (hi res).
-  //getAccXYZ(&x, &y, &z, false); //get accelerometer readings in fast mode (low res).
-  //Serial.print(x);
-  //Serial.print(" ");
-  //Serial.print(y);
-  //Serial.print(" ");
-  //Serial.print(z);
-  //Serial.println();
 
- //Serial.print("{\"i2c\":{\"accel\":\"x\":");
+ 
+  
+	//aJson.addItemToObject(root, "temperature", fmt = aJson.createObject());
+	//aJson.addStringToObject(fmt,"type",		"rect");
+	//aJson.addNumberToObject(fmt,"width",		1920);
+	//aJson.addNumberToObject(fmt,"height",		1080);
+	//aJson.addFalseToObject (fmt,"interlace");
+	//aJson.addNumberToObject(fmt,"frame rate",	24);
+	//Serial.println(aJson.print(root));
+	//aJson.deleteItem(root);
+	//freeMem("the memory: ");
+	delay(100);
+	
+  testObjects();
+
+
+ //Serial.print("{\"i2c\":[{\"accelerometer\":{\"x\":");
  //Serial.print(x);
  //Serial.print(",\"y\":");
  //Serial.print(y);
  //Serial.print(",\"z\":");
  //Serial.print(z);
- //Serial.println("},{\"temp\":5}}");
- Serial.print("{\"i2c\":[{\"accelerometer\":{\"x\":");
- Serial.print(x);
- Serial.print(",\"y\":");
- Serial.print(y);
- Serial.print(",\"z\":");
- Serial.print(z);
- Serial.print("}},{\"temperature\":");
+ //Serial.print("}},{\"temperature\":");
  
- Serial.print(sen.getTemperature());
- Serial.println("0}]}");
+ //Serial.print(sen.getTemperature());
+ //Serial.println("0}]}");
 
- char* sensorType = sen.idTheType(analogRead(IDPinCon1));
-	int senval1 = sen.getSensorValue(1, sensorType);
-	Serial.print("-------------->");
-	Serial.println(senval1);
+ //char* sensorType = sen.idTheType(analogRead(IDPinCon1));
+ //   int senval1 = sen.getSensorValue(1, sensorType);
+ //   Serial.print("-------------->");
+ //   Serial.println(senval1);
 
+}
+
+//Code to print out the free memory
+
+struct __freelist {
+  size_t sz;
+  struct __freelist *nx;
+};
+
+extern char * const __brkval;
+extern struct __freelist *__flp;
+
+uint16_t freeMem(uint16_t *biggest)
+{
+  char *brkval;
+  char *cp;
+  unsigned freeSpace;
+  struct __freelist *fp1, *fp2;
+
+  brkval = __brkval;
+  if (brkval == 0) {
+    brkval = __malloc_heap_start;
+  }
+  cp = __malloc_heap_end;
+  if (cp == 0) {
+    cp = ((char *)AVR_STACK_POINTER_REG) - __malloc_margin;
+  }
+  if (cp <= brkval) return 0;
+
+  freeSpace = cp - brkval;
+
+  for (*biggest = 0, fp1 = __flp, fp2 = 0;
+     fp1;
+     fp2 = fp1, fp1 = fp1->nx) {
+      if (fp1->sz > *biggest) *biggest = fp1->sz;
+    freeSpace += fp1->sz;
+  }
+
+  return freeSpace;
+}
+
+uint16_t biggest;
+
+void freeMem(char* message) {
+  Serial.print(message);
+  Serial.print(":\t");
+  Serial.println(freeMem(&biggest));
+}
+
+// given a PROGMEM string, use Serial.print() to send it out
+// this is needed to save precious memory
+//htanks to todbot for this http://todbot.com/blog/category/programming/
+void printProgStr(const prog_char* str) {
+  char c;
+  if (!str) {
+    return;
+  }
+  while ((c = pgm_read_byte(str))) {
+    Serial.print(c, byte(0));
+    str++;
+  }
 }
