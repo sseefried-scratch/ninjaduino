920d62367b301e6d737562d55e10b0dd03a1863c
Mark Wotton <mwotton@gmail.com>
2012-03-13 12:50:57 +1100

    worker files

---
+++ seabeagle/worker.c
@@ -0,0 +1,5 @@
+#include <czmq.h>
+
+void worker(void * cvoid, zctx_t * context, void * pipe) {
+  puts("unimplemented\n");
+}
---
+++ seabeagle/worker.h
@@ -0,0 +1,10 @@
+#ifndef __WORKER_H__
+#define __WORKER_H__
+
+void worker(void * cvoid, zctx_t * context, void * pipe);
+
+#endif
+
+
+
+
