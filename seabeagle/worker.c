c36d1d8ea80c760d70a8315530673898a89aa6df
Mark Wotton <mwotton@gmail.com>
2012-03-13 16:57:20 +1100

    hardcode broker, cos i is a bad boy.

--- seabeagle/worker.c
+++ seabeagle/worker.c
@@ -4,7 +4,7 @@
 
 void worker(void * cvoid, zctx_t * context, void * pipe) {
   child_handshake(pipe);
-  mdwrk_t *session = mdwrk_new ("tcp://localhost:5555", "echo", 1);
+  mdwrk_t *session = mdwrk_new ("tcp://10.10.50.60:5555", "echo", 1);
   zmsg_t *reply = NULL;
   while (1) {
     zmsg_t *request = mdwrk_recv (session, &reply);
