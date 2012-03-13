420bf98caad4ac52d060370f7fd44841ab7c6235
Mark Wotton <mwotton@gmail.com>
2012-03-13 17:03:06 +1100

    some logging for worker

--- seabeagle/worker.c
+++ seabeagle/worker.c
@@ -3,8 +3,10 @@
 #include "utils.h"
 
 void worker(void * cvoid, zctx_t * context, void * pipe) {
-  child_handshake(pipe);
+
   mdwrk_t *session = mdwrk_new ("tcp://10.10.50.60:5555", "echo", 1);
+  zclock_log("worker connected!");
+  child_handshake(pipe);
   zmsg_t *reply = NULL;
   while (1) {
     zmsg_t *request = mdwrk_recv (session, &reply);
