6136da69c7da02da91e91f7ea314a0974f0b1154
Mark Wotton <mwotton@gmail.com>
2012-03-23 21:28:54 +1100

    more robust camera

--- seabeagle/camera.c
+++ seabeagle/camera.c
@@ -4,7 +4,8 @@
 #include "config.h"
 
 int camera_connected() {
-  return 1;
+  struct stat   buffer;   
+  return (stat ("/dev/video0", &buffer) == 0);
 }
 
 
@@ -15,19 +16,30 @@ void camera(config_t * config) {
   void * data = malloc(2000000); // 2 mb enough?
   sprintf(workername, "%s:camera", config->identity);
   zclock_log("worker %s trying to connect!", workername);
-  mdwrk_t * session = mdwrk_new (config->broker_endpoint, workername, 0);
+
   zmsg_t *reply = NULL;
   while(1) {
-    zmsg_t *request = mdwrk_recv (session, &reply);
-    if (request == NULL)
-      break;              //  Worker was interrupted
-    reply = zmsg_new();
-    if(camera_connected()) {
-      char * command = zmsg_popstr(request);
 
+    while(!camera_connected()) {
+      sleep(1);
+    }
+    // camera is now connected
+    mdwrk_t * session = mdwrk_new (config->broker_endpoint, workername, 0);    
+
+    while(camera_connected()) {
+      
+      zmsg_t *request = mdwrk_recv (session, &reply);
+      if (request == NULL) {
+        mdwrk_destroy(&session);
+        break;
+      }
+
+      reply = zmsg_new();
+      char * command = zmsg_popstr(request);
       if (strcmp(command, "TakePicture") == 0) {
-        // if (!system("rm snap.jpg; uvccapture")) {
-        if (0!=system("rm snap.jpg; uvccapture")) {
+        // still a race condition here. would be lovely if uvccapture
+        // didn't segfault. FIX when we have time
+        if (0!=system("rm snap.jpg; [ -e /dev/video0 ] &&  uvccapture")) {
           zclock_log("error taking photo");
         } else {
           
@@ -40,14 +52,9 @@ void camera(config_t * config) {
       int bytes_read = fread(data, 1, 2000000, f);
       zmsg_pushmem(reply, data, bytes_read);
       zmsg_destroy(&request);
-    } else {
-      zclock_log("camera was unplugged while we were waiting for a request");
-      zmsg_pushstr(reply, "no camera");
     }
-    zmsg_destroy(&request);
-
+    mdwrk_destroy (&session);
+    zclock_log("camera became unavailable");
+    zmsg_pushstr(reply, "no camera");
   }
-  mdwrk_destroy (&session);
-  
-  return;
 }
