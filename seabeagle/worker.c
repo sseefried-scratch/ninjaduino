2786771a05f63f424090228b6cbecd74f00ae0a1
Mark Wotton <mwotton@gmail.com>
2012-03-26 19:14:35 +1100

    lots more sqlite3 code for rules reloading

--- seabeagle/worker.c
+++ seabeagle/worker.c
@@ -1,4 +1,5 @@
 #include <czmq.h>
+#include <sqlite3.h>
 #include "mdwrkapi.h"
 #include "utils.h"
 #include "trigger.h"
@@ -71,18 +72,122 @@
 
  */
 
+/*
+
+  Also, each worker has its own sqlite database. Thus, it can slurp up
+  the rules when it starts up again.
+
+  in the fullness of time, it should also check with the back end and
+  reload anything it doesn't know about. This is a useful hack for
+  now, though.
+
+*/
+
+
+sqlite3 * init_db(char * servicename) {
+  int rc;
+  sqlite3 * db;
+  char *zErrMsg = 0;
+  char * buf = malloc(strlen(servicename) + 4);
+  sprintf(buf, "%s.db", servicename);
+  rc = sqlite3_open(buf, &db);
+  free(buf);
+  if(rc!=0) {
+    zclock_log("ERROR: couldn't write to %s db: %s", servicename, zErrMsg);
+    return NULL;
+  }
+  // create table if it doesn't exist
+  rc = sqlite3_exec(db, "create table if not exists rules (\
+rule_id string,\
+trigger_name string,\
+target_worker string,\
+auth string,\
+addins string);", NULL, NULL, &zErrMsg);
+
+  return db;
+}
+
+typedef struct {
+  zhash_t * rules;
+  zctx_t * context;
+} rulepackage_t;
+
+
+
+
+// somewhat counterintuitively, this returns NULL if everything's ok.
+char * create_trigger(zhash_t * rules, 
+                      char * rule_id, 
+                      zctx_t * context,
+                      triggerconfig_t * tconf) {
+  void * rule_pipe = zthread_fork(context, trigger, tconf);
+  send_sync("ping", rule_pipe);
+  char * pipe_resp = zstr_recv(rule_pipe);
+  
+
+  if(strcmp(pipe_resp, "pong") == 0) {
+    zhash_insert(rules, rule_id, rule_pipe);
+    free(pipe_resp);
+    return NULL;
+  } else {
+    zclock_log("something went wrong creating a trigger: %s", pipe_resp);
+    return pipe_resp;
+  }
+}
+
+int load_rule(void *rvoid, int argc, char ** argv,  char ** column) {
+  int i;
+  for(i=0;i<argc;i++) {
+    printf("Argument %d is %s\n", i, argv[i]);
+  }
+  rulepackage_t * rpkg = (rulepackage_t *) rvoid;
+  triggerconfig_t * tconf = malloc(sizeof(triggerconfig_t));
+  tconf->rule_id = argv[0];
+  tconf->trigger_name = argv[1];
+  tconf->target_worker = argv[2];
+
+  create_trigger(rpkg->rules, argv[0], rpkg->context, tconf);
+}
+
+void reload_rules(zctx_t * context, sqlite3 * db, char * servicename, zhash_t * rules) {
+
+  char *zErrMsg = 0;
+  int rc;
+  rulepackage_t rpkg = {rules, context};
+  rc=sqlite3_exec(db, "select rule_id,trigger_name,target_worker,auth,addins from rules", load_rule, &rpkg, &zErrMsg);
+  if(rc!=0) {
+    // not necessarily a problem - if we've never written to the db,
+    // the rules table doesn't exist, which is fine.
+    zclock_log("INFO: couldn't read from %s db: %s", servicename, zErrMsg);
+    return;
+  }
+
+}
+
 
 void generic_worker(void * cvoid, zctx_t * context, void * pipe) {
   workerconfig_t *config = (workerconfig_t*) cvoid;
   zhash_t * rules = zhash_new();
+
   child_handshake(pipe);
   zmsg_t *reply = NULL;
   void * rule_pipe = NULL;
   char * ninja = config->base_config->identity;
   char * channel = config->channel;
+
   char * servicename = malloc(strlen(ninja) +
                               strlen(channel) + 2);
   sprintf(servicename, "%s:%s", ninja,channel);
+
+  // sqlite stuff
+  int rc;
+  char * tail = NULL;
+  sqlite3 *db = init_db(servicename);
+  sqlite3_stmt * insert_stmt;
+  sqlite3_prepare_v2(db, "insert into rules VALUES (@RULEID, @TRIGGER_NAME, @TARGET_WORKER, @AUTH, @ADDINS);", 512, &insert_stmt, NULL);
+
+  reload_rules(context, db, servicename, rules);
+
   mdwrk_t *session = mdwrk_new (config->base_config->broker_endpoint, servicename, 0);
   zclock_log("%s worker connected!", servicename);
 
@@ -109,20 +214,25 @@ void generic_worker(void * cvoid, zctx_t * context, void * pipe) {
       } else {
         triggerconfig_t * tconf = malloc(sizeof(triggerconfig_t));
         create_triggerconfig(tconf, request, channel, rule_id);
+        char * created = create_trigger(rules, rule_id, context, tconf);
+        if(NULL == created) {
+          // happy path, so add to db
+          sqlite3_bind_text(insert_stmt, 1, tconf->rule_id, -1, SQLITE_TRANSIENT);
+          sqlite3_bind_text(insert_stmt, 2, tconf->trigger_name, -1, SQLITE_TRANSIENT);
+          sqlite3_bind_text(insert_stmt, 3, tconf->target_worker, -1, SQLITE_TRANSIENT);
+          sqlite3_bind_blob(insert_stmt, 4, zframe_data(tconf->auth), zframe_size(tconf->auth), SQLITE_TRANSIENT);
+          sqlite3_bind_blob(insert_stmt, 5, zframe_data(tconf->addins), zframe_size(tconf->addins), SQLITE_TRANSIENT);
+          sqlite3_step(insert_stmt);
+          sqlite3_clear_bindings(insert_stmt);
+          sqlite3_reset(insert_stmt);
 
-        rule_pipe = zthread_fork(context, trigger, tconf);
-        send_sync("ping", rule_pipe);
-        char * pipe_resp = zstr_recv(rule_pipe);
-
-
-        if(strcmp(pipe_resp, "pong") == 0) {
-          zhash_insert(rules, rule_id, rule_pipe);
           zmsg_pushstr(reply, "ok");
         } else {
-          zclock_log("something went wrong creating a trigger: %s", pipe_resp);
-          zmsg_pushstr(reply, pipe_resp);
+          zclock_log("create_trigger failed: %s", created);
+          zmsg_pushstr(reply, created);
         }
-        free(pipe_resp);
+        free(created);
+
       }
     } else if (strcmp(command,"RemoveRule") == 0) {
 
